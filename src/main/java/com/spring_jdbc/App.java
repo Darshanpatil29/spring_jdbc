38dcd3309990105e1752c06b38f56f5c0d31f8e6
Darshan Patil <darshanpatil972@gmail.com>
2025-05-14 11:50:16 +0530

    Meaningful commit message

---
+++ src/main/java/com/spring_jdbc/App.java
@@ -0,0 +1,13 @@
+package com.spring_jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World this is Spring JDBC Tutorial!" );
+    }
+}
